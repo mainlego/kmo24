94868ab28f77b2079c23f3dbec8544f7e236baa8
mainlego <vladmelbiz@gmail.com>
2025-11-03 22:00:31 +0200

    fix: Исправлена синтаксическая ошибка в about.vue
    Проблема:
    - Vue компилятор не может обработать атрибут style с кавычками внутри SVG data URL
    - Ошибка: Attribute name cannot contain U+0022 ("), U+0027 ('), and U+003C (<)
    
    Решение:
    - Заменил статический style на :style с Vue binding
    - Использовал template literals и &quot; для экранирования кавычек в SVG data URL
    - Теперь Vue корректно обрабатывает backgroundImage
    
    Файл: pages/about.vue:76

--- frontend/pages/about.vue
+++ frontend/pages/about.vue
@@ -73,7 +73,7 @@
     <section class="py-20 px-4 relative">
       <!-- Background Pattern -->
       <div class="absolute inset-0 opacity-5">
-        <div class="absolute inset-0" style="background-image: url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%239C92AC" fill-opacity="0.4"%3E%3Cpath d="M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E');"></div>
+        <div class="absolute inset-0" :style="{ backgroundImage: `url('data:image/svg+xml,%3Csvg width=&quot;60&quot; height=&quot;60&quot; viewBox=&quot;0 0 60 60&quot; xmlns=&quot;http://www.w3.org/2000/svg&quot;%3E%3Cg fill=&quot;none&quot; fill-rule=&quot;evenodd&quot;%3E%3Cg fill=&quot;%239C92AC&quot; fill-opacity=&quot;0.4&quot;%3E%3Cpath d=&quot;M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z&quot;/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')` }"></div>
       </div>
 
       <div class="max-w-7xl mx-auto relative">
